bbd16d232a17b0b1677beee21f159dac70068f0f
FearlessSwiftie <fearlessswiftie1322@gmail.com>
2017-03-12 13:35:35 -0700

    added new user commands
    *added try/catch statements to stop the bot from crashing in certain situations
    *redid the modlog function to reduce PM spam
    *other cool stuffs

--- main.js
+++ main.js
@@ -3,39 +3,37 @@ const config = require("./auth.json");
 
 const arianaBot = new Discord.Client();
 
-var version = "2017.02.28a";
+var version = "2017.03.12b";
 
 arianaBot.on ("ready", () => {
     console.log("Logged in as " + arianaBot.user.username + " - " + arianaBot.user.id + "\nReady!");
 });
 
-arianaBot.on ('message', message => {
+arianaBot.on ("message", message => {
     var command = message.content.split(" ");
     var params = command.slice(1, command.length).join(" ");
 
+    var target = message.mentions.users.first();
+
     switch(command[0].toLowerCase()) {
+        case "!avatar":
+            userAvatar(message);
+            break;
         case "!commands":
         case "!help":
-	    message.channel.sendMessage("__My Commands__:\n!commands, !help: Displays my available commands.\n!rules: Displays the rules" + 
-            "\n!setalbum: Grants you a role based on what your favorite album is.\n!version: Displays the current version the bot is running on.");
+	    message.channel.sendMessage("__My Commands__:\n```json\n\"!avatar\": Posts a link to your avatar. Mention a user to get their avatar." +
+            "\n\"!commands, !help\": Displays my available commands.\n\"!rules\": Displays the rules" + 
+            "\n\"!setalbum\": Grants you a role based on what your favorite album is.\n\"!version\": Displays the current version the bot is running on." +
+            "\n\"!serverinfo, !sinfo\": Displays the server's information." +
+            "\n\"!userinfo, !uinfo\": Displays your account's information. Mention a user to see their account information.\n```");
 	    break;
-        case "!eval":
-            if (message.author.id == config.ownerID) {
-                if (command[1] == null) {
-                    message.reply ("You have not specified what you want me to evaluate! :(");
-                } else {
-                    try {
-                        var evaled = eval(params);
-                        if (typeof evaled !== "string")
-                            evaled = require("util").inspect(evaled);
-                            message.channel.sendMessage("```xl\n" + clean(evaled) + "\n```");
-                    } catch (err) {
-                        message.channel.sendMessage("`ERROR` ```xl\n" + clean(err) + "\n```");
-                    }
-                }
-            } else {
-                message.reply("lol no :rolling_eyes:");
-            }
+        case "!serverinfo":
+        case "!sinfo":
+            var server = message.guild;
+            message.channel.sendMessage("```xl\nServer Name: " + server.name +
+            "\nServer ID: " + server.id + "\nServer Owner: " + server.owner.user.username +
+            "\nMembers: " + server.memberCount + "\nChannels: " + server.channels.size +
+            "\nRegion: " + server.region +  "```\n" + server.iconURL);
             break;
         case "!rules":
             message.channel.sendMessage("__Rules__:\n1) Don't be a dick\n2) No NSFW pictures\n3) No spamming pictures, however " +
@@ -45,50 +43,66 @@ arianaBot.on ('message', message => {
         case "!setalbum":
             favoriteAlbum(message);
             break;
+        case "!userinfo":
+        case "!uinfo":
+            userInfo(message);
+            break;
         case "!version":
             message.channel.sendMessage("Current version: " + version);
             break;
 	//mod commands
         case "!ban":
-	    var userToBan = message.mentions.users.first();
-	    if(isMod(message)) {
-                message.guild.member(userToBan).ban();
-		message.reply(userToBan.username + " was banned from the server! :open_mouth:");
-		modlog(userToBan.username + " was banned from the server by " + message.author.username + ".");
-	    } else {
-	        message.reply("lol no :rolling_eyes:");
-	    }
+            try {
+	        if(isMod(message)) {
+                    message.guild.member(target).ban();
+		    message.reply(target.username + " was banned from the server! :open_mouth:");
+		    modlog(target.username + " was banned from the server by " + message.author.username + ".");
+	        } else {
+	            message.reply("lol no :rolling_eyes:");
+	        } }  catch(err) {
+                    console.log(err);
+                    message.reply("user `" + params + "` was not found on the server. :frowning:");
+                }
 	    break;
 	case "!kick":
-	    var userToKick = message.mentions.users.first();
-	    if(isMod(message)) {
-                message.guild.member(userToKick).kick();
-		message.reply(userToKick.username + " was kicked from the server! :open_mouth:");
-		modlog(userToKick.username + " was kicked from the server by " + message.author.username + ".");
-	    } else {
-	        message.reply("lol no :rolling_eyes:");
-	    }
+            try {
+	        if(isMod(message)) {
+                    message.guild.member(target).kick();
+		    message.reply(target.username + " was kicked from the server! :open_mouth:");
+		    modlog(target.username + " was kicked from the server by " + message.author.username + ".");
+	        } else {
+	            message.reply("lol no :rolling_eyes:");
+	        } } catch(err) {
+                    console.log(err);
+                    message.reply("user `" + params + "` was not found on the server. :frowning:");
+                }
 	    break;
 	case "!mute":
-	    var userToMute = message.mentions.users.first();
-	    if (isMod(message)) {
-	        message.channel.overwritePermissions(userToMute, {SEND_MESSAGES: false });
-		message.reply(userToMute.username + " was muted! :open_mouth:");
-		modlog(userToMute.username + " was muted by " + message.author.username + " in " + message.channel.name + ".");
-	    } else {
-		message.reply("lol no :rolling_eyes:");
-	    }
+            try {
+	        if (isMod(message)) {
+	            message.channel.overwritePermissions(target, {SEND_MESSAGES: false });
+		    message.reply(target.username + " was muted! :open_mouth:");
+		    modlog(target.username + " was muted by " + message.author.username + " in " + message.channel.name + ".");
+	        } else {
+		    message.reply("lol no :rolling_eyes:");
+	        } } catch (err) {
+                    console.log(err);
+                    message.reply("user `" + params + "` was not found on the server. :frowning:");
+                }
 	    break;
 	case "!unmute":
-	    var userToUnmute = message.mentions.users.first();
-	    if (isMod(message)) {
-	        message.channel.overwritePermissions(userToUnmute, {SEND_MESSAGES: null });
-		message.reply(userToUnmute.username + " was unmuted! :open_mouth:");
-		modlog(userToUnmute.username + " was unmuted by " + message.author.username + " in " + message.channel.name + ".");
-	    } else {
-		message.reply("lol no :rolling_eyes:");
-	    }
-	    break
+            try {
+	        if (isMod(message)) {
+	            message.channel.overwritePermissions(target, {SEND_MESSAGES: null });
+		    message.reply(target.username + " was unmuted! :open_mouth:");
+		    modlog(target.username + " was unmuted by " + message.author.username + " in " + message.channel.name + ".");
+	        } else {
+		    message.reply("lol no :rolling_eyes:");
+	        } } catch (err) {
+                    console.log(err);
+                    message.reply("user `" + params + "` was not found on the server. :frowning:");
+                }
+	    break;
 	case "!setstatus":
 	    if (isMod(message)) {
 		arianaBot.user.setGame(params);
@@ -122,7 +136,27 @@ arianaBot.on ('message', message => {
 		log(message.author.username + " attempted to restart me.");
 	    }
 	    break;
-        }
+	//restricted commands
+	case "!eval":
+            if (message.author.id == config.ownerID) {
+                if (command[1] == null) {
+                    message.reply ("You have not specified what you want me to evaluate! :(");
+                } else {
+                    try {
+                        var evaled = eval(params);
+
+                        if (typeof evaled !== "string")
+                            evaled = require("util").inspect(evaled);
+                            message.channel.sendMessage("```xl\n" + clean(evaled) + "\n```");
+                    } catch (err) {
+                        message.channel.sendMessage("`ERROR` ```xl\n" + clean(err) + "\n```");
+                    }
+                }
+            } else {
+                message.reply("lol no :rolling_eyes:");
+            }
+            break;
+    }
 });
 
 arianaBot.on("guildMemberAdd", (member) => {
@@ -163,6 +197,34 @@ function clearAlbum(message) {
     });
 }
 
+function convertTimestamp(timestamp) {
+/* thanks to kmaida on github for this function   *
+ * source: https://gist.github.com/kmaida/6045266 */
+    var d = new Date(timestamp),
+	yyyy = d.getFullYear(),
+	mm = ("0" + (d.getMonth() + 1)).slice(-2),
+	dd = ("0" + d.getDate()).slice(-2),
+	hh = d.getHours(),
+	h = hh,
+	min = ("0" + d.getMinutes()).slice(-2),
+	ampm = "AM",
+	time;
+			
+	if (hh > 12) {
+	    h = hh - 12;
+	    ampm = "PM";
+	} else if (hh === 12) {
+	    h = 12;
+	    ampm = "PM";
+	} else if (hh == 0) {
+	    h = 12;
+	}
+	
+	time = yyyy + "-" + mm + "-" + dd + ", " + h + ":" + min + " " + ampm;
+		
+	return time;
+}
+
 function favoriteAlbum(message) {
     var command = message.content.split(" ");
     var params = command.slice(1, command.length).join(" ");
@@ -200,6 +262,9 @@ function favoriteAlbum(message) {
 	    message.reply("your favorite album has been cleared. :frowning:");
 	    log(message.author.username + " has cleared their favorite album. Appropriate roles have been removed.");
 	    break;
+        default:
+            message.reply("woah there! That's not an Ariana album. :confused:");
+            break;
     }
 }
 
@@ -208,17 +273,55 @@ function isMod(message) {
 }
 
 function log(message) {
-    arianaBot.users.get("147109473155022848").sendMessage(message); //Fearless Swiftie
+    arianaBot.users.get("147109473155022848").sendMessage(message);
     console.log(message);
 }
 
 function modlog(message) {
-    arianaBot.users.get("147109473155022848").sendMessage(message); //Fearless Swiftie
-    arianaBot.users.get("163245022181982208").sendMessage(message); //joeyer5
-    arianaBot.users.get("118197472232341511").sendMessage(message); //SavoyRoad
-    arianaBot.users.get("115631289201065993").sendMessage(message); //Compressirk
-    arianaBot.users.get("244629699521675265").sendMessage(message); //Lindsey
+    arianaBot.channels.get("290575930131349514").sendMessage(message);
     console.log(message);
+}
+
+function userAvatar(message) {
+    var command = message.content.split(" ");
+    var target = message.mentions.users.first();
+    var params = command.slice(1, command.length).join(" ");
+
+    var user = message.author;
+
+    if(command[1] == null) {
+        message.reply("your avatar is " + user.avatarURL + ".");
+    } else {
+        try {
+            message.reply(target.username + "'s avatar is " + target.avatarURL + ".");
+        } catch (err) {
+            console.log(err);
+            message.reply("user `" + params + "` was not found on the server. :frowning:");
+        }
+    }
+}
+
+function userInfo(message) {
+    var command = message.content.split(" ");
+    var target = message.mentions.users.first();
+    var params = command.slice(1, command.length).join(" ");
+
+    var user = message.author;
+
+    if(command[1] == null) {
+        message.channel.sendMessage("```xl\nUsername: " + user.username + " #" + user.discriminator +
+        "\nID: " + user.id + "\nCreated: " + convertTimestamp(user.createdTimestamp) +
+        "\nJoined: " + convertTimestamp(message.member.joinedTimestamp) + "\n```" + user.avatarURL);
+    } else {
+        try {
+            message.channel.sendMessage("```xl\nUsername: " + target.username + " #" + target.discriminator +
+            "\nID: " + target.id + "\nCreated: " + convertTimestamp(target.createdTimestamp) +
+            "\nJoined: " + convertTimestamp(message.guild.member(target).joinedTimestamp) + "\n```" + target.avatarURL);
+        } catch (err) {
+            console.log(err);
+            message.reply("user `" + params + "` was not found on the server. :frowning:");
+        }
+    }
 }
 
 arianaBot.login(config.token);
